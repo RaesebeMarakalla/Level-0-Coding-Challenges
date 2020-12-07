32bb4c01a467f95d05528eb35988d186fec488e1
RaesebeMarakalla <raesebemmarakalla@gmail.com>
2020-12-07 15:20:34 +0200

    added task3

--- task3.js
+++ task3.js
@@ -1,9 +1,9 @@
-function twoNum(a, b) {
+function sumOfTwoNum(a, b) {
   if ((a == 65) || (b == 65) || ((a + b) == 65)) {
     return true;
   }
   return false;
 }
 
-console.log(twoNum(33, 32));
-console.log(twoNum(43, 56));
+console.log(sumOfTwoNum(33, 32));
+console.log(sumOfTwoNum(43, 56));
