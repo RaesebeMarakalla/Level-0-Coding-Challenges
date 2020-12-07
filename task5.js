a4d80886b8125a06909ef2e7b2ff23393f63e5dc
RaesebeMarakalla <raesebemmarakalla@gmail.com>
2020-12-07 15:39:03 +0200

    added task5

--- task5.js
+++ task5.js
@@ -1,6 +1,7 @@
-var sidea = 3;
-var sideb = 4;
-var sidec = 5;
-var perimeter = (sidea +  sideb + sidec) / 2;
-var area =  Math.sqrt(perimeter * ((perimeter - sidea) * (perimeter - sideb) * (perimeter - sidec)));
-console.log(area);
+function triangleArea(a, b , c) {
+  var perimeter = (a +  b + c) / 2;
+  var area =  Math.sqrt(perimeter * ((perimeter - a) * (perimeter - b) * (perimeter - c)));
+  console.log(area);
+}
+
+triangleArea(10, 12 ,14);
