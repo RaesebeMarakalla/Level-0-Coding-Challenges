291882b688778c03817bef0aab1f24aaa22932e5
RaesebeMarakalla <raesebemmarakalla@gmail.com>
2020-12-07 15:36:39 +0200

    added task4

--- task4.js
+++ task4.js
@@ -1,4 +1,4 @@
-function twoNumbers(a, b) {
+function checkEquality(a, b) {
   if (a == 3 || b == 3) {
     var sumStr = (a + b).toString();
     for(var i=0; i < sumStr.length; ++i)
@@ -11,5 +11,5 @@ function twoNumbers(a, b) {
   }
   return false;
 }
-console.log(twoNumbers(3,0));
-console.log(twoNumbers(674,35));
+console.log(checkEquality(3,0));
+console.log(checkEquality(674,35));
